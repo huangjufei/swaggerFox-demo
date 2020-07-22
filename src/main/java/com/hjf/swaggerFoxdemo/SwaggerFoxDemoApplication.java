b998bc3121c5c3414492bed6580a46b098d7beb4
huangjufei <28160997@qq.com>
2020-07-22 10:31:21 +0800

    first commit

---
+++ src/main/java/com/hjf/swaggerFoxdemo/SwaggerFoxDemoApplication.java
@@ -0,0 +1,13 @@
+package com.hjf.swaggerFoxdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwaggerFoxDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SwaggerFoxDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hjf/swaggerFoxdemo/SwaggerFoxDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hjf.swaggerFoxdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SwaggerFoxDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
